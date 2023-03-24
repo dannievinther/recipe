3564ead86ff37dbdf7ad5fb5a0fcff9c5d0f1794
Dannie Vinther <davi@kea.dk>
2023-03-24 23:27:36 +0100

    add trailing slash from url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "never",
+  trailingSlash: "always",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
