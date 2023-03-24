eeb4d17291f4317db55dc6f99aeea9d181e18d4e
Dannie Vinther <davi@kea.dk>
2023-03-24 15:03:40 +0100

    first

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx()]
+});
