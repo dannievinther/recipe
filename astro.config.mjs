6390733aa35aa781a76e2cce31007a430521b0af
Dannie Vinther <davi@kea.dk>
2023-04-07 12:44:24 +0200

    add Astro Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), compress()]
 });
