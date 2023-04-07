df40750d9b81a989357866515f2b5f59d584311f
Dannie Vinther <davi@kea.dk>
2023-04-07 13:32:05 +0200

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
